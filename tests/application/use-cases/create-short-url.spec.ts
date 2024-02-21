c04e4090de32805d847137353e50b03626f0479c
antoniomrrds <arqueiroverd@gmail.com>
2024-02-21 10:15:25 -0300

    test: 🧪make sure CreateShortURL calls IUniqueIDGenerator's generateUniqueId method once

--- tests/application/use-cases/create-short-url.spec.ts
+++ tests/application/use-cases/create-short-url.spec.ts
@@ -3,8 +3,10 @@ import { IUniqueIDGenerator } from '@/application/ports/crypto'
 
 class UniqueIDGeneratorStub implements IUniqueIDGenerator {
   output = 'any_unique_id'
+  callsCount = 0
 
   generateUniqueId (): IUniqueIDGenerator.Output {
+    this.callsCount++
     return this.output
   }
 }
@@ -17,5 +19,7 @@ describe('CreateShortURL', () => {
     await sut.perform({ url: 'any_url' })
 
     expect(uniqueIDGeneratorStub.output).toBe('any_unique_id')
+    expect(uniqueIDGeneratorStub.callsCount).toBe(1)
   })
+  it
 })
