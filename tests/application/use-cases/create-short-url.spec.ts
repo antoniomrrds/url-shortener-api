2bf7e81dd645e082f3fdee3a062f00713fa7eb16
antoniomrrds <arqueiroverd@gmail.com>
2024-02-21 10:39:37 -0300

    refactor: 📦 move sut creation to a factory helper method

--- tests/application/use-cases/create-short-url.spec.ts
+++ tests/application/use-cases/create-short-url.spec.ts
@@ -2,7 +2,7 @@ import { CreateShortUrl } from '@/application/use-cases'
 import { IUniqueIDGenerator } from '@/application/ports/crypto'
 
 class UniqueIDGeneratorStub implements IUniqueIDGenerator {
-  output = 'any_unique_id'
+  public output = 'any_unique_id'
   callsCount = 0
 
   generateUniqueId (): IUniqueIDGenerator.Output {
@@ -11,15 +11,24 @@ class UniqueIDGeneratorStub implements IUniqueIDGenerator {
   }
 }
 
+type SutTypes = {
+  sut: CreateShortUrl
+  uniqueIDGeneratorStub: UniqueIDGeneratorStub
+}
+
+const makeSut = (): SutTypes => {
+  const uniqueIDGeneratorStub = new UniqueIDGeneratorStub()
+  const sut = new CreateShortUrl(uniqueIDGeneratorStub)
+  return { sut, uniqueIDGeneratorStub }
+}
+
 describe('CreateShortURL', () => {
   it('Should call generateUniqueId method of IUniqueIDGenerator when perform is invoked', async () => {
-    const uniqueIDGeneratorStub = new UniqueIDGeneratorStub()
-    const sut = new CreateShortUrl(uniqueIDGeneratorStub)
+    const { sut, uniqueIDGeneratorStub } = makeSut()
 
     await sut.perform({ url: 'any_url' })
 
     expect(uniqueIDGeneratorStub.output).toBe('any_unique_id')
     expect(uniqueIDGeneratorStub.callsCount).toBe(1)
   })
-  it
 })
