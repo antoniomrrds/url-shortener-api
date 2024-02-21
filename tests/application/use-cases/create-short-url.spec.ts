cc1f92c5fb095d13ba20dc92edf24ff3c3ff523d
antoniomrrds <arqueiroverd@gmail.com>
2024-02-21 10:10:47 -0300

    test: 🧪 make sure CreateShortURL calls the generateUniqueId method of IUniqueIDGenerator when perform is invoked

---
+++ tests/application/use-cases/create-short-url.spec.ts
@@ -0,0 +1,21 @@
+import { CreateShortUrl } from '@/application/use-cases'
+import { IUniqueIDGenerator } from '@/application/ports/crypto'
+
+class UniqueIDGeneratorStub implements IUniqueIDGenerator {
+  output = 'any_unique_id'
+
+  generateUniqueId (): IUniqueIDGenerator.Output {
+    return this.output
+  }
+}
+
+describe('CreateShortURL', () => {
+  it('Should call generateUniqueId method of IUniqueIDGenerator when perform is invoked', async () => {
+    const uniqueIDGeneratorStub = new UniqueIDGeneratorStub()
+    const sut = new CreateShortUrl(uniqueIDGeneratorStub)
+
+    await sut.perform({ url: 'any_url' })
+
+    expect(uniqueIDGeneratorStub.output).toBe('any_unique_id')
+  })
+})
