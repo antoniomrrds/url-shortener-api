a16cbb520b253e6a1b94ee28c0cc134b4f1eecbb
antoniomrrds <arqueiroverd@gmail.com>
2024-02-22 01:35:55 -0300

    test: 🧪 ensure that UniqueIDGeneratorAdapter returns the unique ID

--- tests/infrastructure/crypto/unique-id-adapter.spec.ts
+++ tests/infrastructure/crypto/unique-id-adapter.spec.ts
@@ -1,17 +1,30 @@
-import { UniqueIDAdapter } from '@/infrastructure/crypto'
+import { UniqueIDGeneratorAdapter } from '@/infrastructure/crypto'
 import { nanoid } from 'nanoid'
 
 jest.mock('nanoid')
 
 describe('UniqueIdAdapter', () => {
-  it('Should call the nanoid method', () => {
+  it('Should call the nanoId method', () => {
     // Arrange
-    const sut = new UniqueIDAdapter()
+    const sut = new UniqueIDGeneratorAdapter()
 
     // Act
     sut.generateUniqueId()
 
     // Assert
     expect(nanoid).toHaveBeenCalledTimes(1)
+  })
+  it('Should return the unique id', () => {
+    // Arrange
+    const sut = new UniqueIDGeneratorAdapter()
+    const expected = 'any_unique_id'
+    const nanoidSpy = nanoid as jest.Mock
+    nanoidSpy.mockReturnValueOnce(expected)
+
+    // Act
+    const uniqueId = sut.generateUniqueId()
+
+    // Assert
+    expect(uniqueId).toBe(expected)
   })
 })
