02dae13c15b91c0d43b99dde83c0087c260a53d0
antoniomrrds <arqueiroverd@gmail.com>
2024-02-22 21:00:36 -0300

    test: 🧪ensure CreateShortUrlController returns 400 if originalUrl field is undefined

--- tests/presentation/controllers/create-short-url-controller.spec.ts
+++ tests/presentation/controllers/create-short-url-controller.spec.ts
@@ -11,4 +11,14 @@ describe('CreateShortUrlController', () => {
       data: new Error('The field originalUrl is required.')
     })
   })
+  it('Should return 400 if originalUrl field is undefined', async () => {
+    const sut = new CreateShortUrlController()
+
+    const httpResponse = await sut.handleRequest({ originalUrl: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field originalUrl is required.')
+    })
+  })
 })
