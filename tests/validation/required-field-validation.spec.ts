58cdcd2ee81bad845991161db2fbe7b00c92d172
antoniomrrds <arqueiroverd@gmail.com>
2024-02-24 01:06:10 -0300

    test: 🧪 ensure RequiredFieldValidation returns undefined if value is not empty

--- tests/validation/required-field-validation.spec.ts
+++ tests/validation/required-field-validation.spec.ts
@@ -4,16 +4,24 @@ import { RequiredFieldValidation } from '@/presentation/validation'
 
 describe('RequiredFieldValidation', () => {
   const testCases = [
-    { name: 'empty', expected: '' },
-    { name: 'null', expected: null },
-    { name: 'undefined', expected: undefined }
+    { name: 'is empty', expected: '' },
+    { name: 'contains space', expected: '  ' },
+    { name: 'is null', expected: null },
+    { name: 'is undefined', expected: undefined }
   ]
 
-  it.each(testCases)('Should return MissingParamError if value is $name', ({ expected }) => {
+  it.each(testCases)('Should return MissingParamError if value $name', ({ expected }) => {
     const sut = new RequiredFieldValidation(expected as any, 'any_field')
 
     const error = sut.validate()
 
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+  it('Should return undefined if value is not empty', () => {
+    const sut = new RequiredFieldValidation('any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
