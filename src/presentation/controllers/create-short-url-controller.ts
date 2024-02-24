d27b2de9e0670564a0981a82e17d207f0bb94204
antoniomrrds <arqueiroverd@gmail.com>
2024-02-24 19:11:48 -0300

    feat: ​​🧩apply composite

--- src/presentation/controllers/create-short-url-controller.ts
+++ src/presentation/controllers/create-short-url-controller.ts
@@ -1,7 +1,7 @@
 import { ICreateShortUrlUseCase } from '@/domain/use-cases'
 import { InvalidParamError } from '@/presentation/errors'
 import { badRequest, created, serverError } from '@/presentation/helpers'
-import { IUrlValidator, RequiredFieldValidation } from '@/presentation/validation'
+import { IUrlValidator, RequiredFieldValidation, ValidationComposite } from '@/presentation/validation'
 import { HttpResponse } from '@/presentation/ports'
 
 type CreateShortUrlRequest = {
@@ -40,7 +40,8 @@ export class CreateShortUrlController {
   }
 
   private validateRequest ({ originalUrl }: CreateShortUrlRequest): Error | undefined {
-    const validator = new RequiredFieldValidation(originalUrl, 'originalUrl')
-    return validator.validate()
+    return new ValidationComposite(
+      [new RequiredFieldValidation(originalUrl, 'originalUrl')
+      ]).validate()
   }
 }
