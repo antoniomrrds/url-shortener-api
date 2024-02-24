ced5cef840331ac25327b2752f0f1458970c23a2
antoniomrrds <arqueiroverd@gmail.com>
2024-02-24 18:21:14 -0300

    feat: ​​🧩 ensure ValidationComposite returns the first error if more than one Validator fails

--- src/presentation/validation/validation-composite.ts
+++ src/presentation/validation/validation-composite.ts
@@ -1,9 +1,14 @@
 import { IValidator } from '@/presentation/validation/ports'
 
-export class ValidationComposite {
+export class ValidationComposite implements IValidator {
   constructor (private readonly validators: IValidator[]) {}
 
-  validate (): undefined {
-    return undefined
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) {
+        return error
+      }
+    }
   }
 }
