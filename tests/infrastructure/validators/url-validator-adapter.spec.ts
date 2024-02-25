c1d1bafdf8b958e2492afa2b9498f211513e86ca
antoniomrrds <arqueiroverd@gmail.com>
2024-02-25 19:25:59 -0300

    refactor: 📦 move sut creation to a factory helper method

--- tests/infrastructure/validators/url-validator-adapter.spec.ts
+++ tests/infrastructure/validators/url-validator-adapter.spec.ts
@@ -3,6 +3,10 @@ import validator from 'validator'
 
 jest.mock('validator')
 
+const makeSut = (): UrlValidatorAdapter => {
+  return new UrlValidatorAdapter()
+}
+
 describe('UrlValidatorAdapter', () => {
   let isValidatorSpy: jest.Mocked<typeof validator>
 
@@ -13,21 +17,21 @@ describe('UrlValidatorAdapter', () => {
 
   it('Should return false if validator returns false', () => {
     isValidatorSpy.isURL.mockReturnValueOnce(false)
-    const sut = new UrlValidatorAdapter()
+    const sut = makeSut()
 
     const isValid = sut.isValid('invalid_url')
 
     expect(isValid).toBe(false)
   })
   it('Should return true if validator returns true', () => {
-    const sut = new UrlValidatorAdapter()
+    const sut = makeSut()
 
     const isValid = sut.isValid('valid_url')
 
     expect(isValid).toBe(true)
   })
   it('Should call validator with correct value', () => {
-    const sut = new UrlValidatorAdapter()
+    const sut = makeSut()
 
     sut.isValid('any_url')
 
