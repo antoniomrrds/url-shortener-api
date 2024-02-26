0fbcf99adb2da9fffb546005810c0938016f57ea
antoniomrrds <arqueiroverd@gmail.com>
2024-02-26 02:17:11 -0300

    test: 🧪ensure that ValidationBuilder invokes the url method and returns a UrlValidation

--- tests/presentation/validation/validation-builder.spec.ts
+++ tests/presentation/validation/validation-builder.spec.ts
@@ -1,4 +1,8 @@
-import { RequiredFieldValidation, ValidationBuilder } from '@/presentation/validation'
+import { UrlValidatorAdapter } from '@/infrastructure/validators'
+import { RequiredFieldValidation, UrlValidation, ValidationBuilder } from '@/presentation/validation'
+import { UrlValidatorSpy } from '@/tests/infrastructure/mocks'
+
+jest.mock('@/infrastructure/validators/url-validator-adapter')
 
 describe('ValidationBuilder', () => {
   it('Should return a RequiredFieldValidation', () => {
@@ -8,5 +12,19 @@ describe('ValidationBuilder', () => {
       .build()
 
     expect(validations).toEqual([new RequiredFieldValidation('any_value', 'any_name')])
+  })
+  it('Should return a UrlValidation', () => {
+    const urlValidatorSpy = UrlValidatorAdapter as jest.MockedClass<typeof UrlValidatorAdapter>
+    urlValidatorSpy.mockImplementation(() => new UrlValidatorSpy())
+
+    const validations = ValidationBuilder
+      .of({ value: 'any_value', fieldName: 'any_name' })
+      .url()
+      .build()
+
+    expect(validations).toEqual([
+      new UrlValidation('any_value', 'any_name', new UrlValidatorSpy())
+    ])
+    expect(urlValidatorSpy).toHaveBeenCalledTimes(1)
   })
 })
