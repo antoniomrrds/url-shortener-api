cd3690306e37cab3338a2bf78dee4ab6c96ceafd
antoniomrrds <arqueiroverd@gmail.com>
2024-02-26 00:15:11 -0300

    feat: ​​🧩ensure UrlValidation returns InvalidParamError if urlValidator returns false

--- src/presentation/validation/url-validation.ts
+++ src/presentation/validation/url-validation.ts
@@ -1,12 +1,17 @@
-import { IUrlValidator } from '@/presentation/validation/ports'
+import { InvalidParamError } from '@/presentation/errors'
+import { IUrlValidator, IValidation } from '@/presentation/validation/ports'
 
-export class UrlValidation {
+export class UrlValidation implements IValidation {
   constructor (
     private readonly value: string,
+    private readonly fieldName: string,
     private readonly urlValidator: IUrlValidator
   ) {}
 
-  validate (): undefined {
-    this.urlValidator.isValid(this.value)
+  validate (): undefined | Error {
+    const isValid = this.urlValidator.isValid(this.value)
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
   }
 }
