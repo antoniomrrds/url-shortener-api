c2c4e942c87eca24fea92185a7fd65f200d8565d
antoniomrrds <arqueiroverd@gmail.com>
2024-02-27 00:10:19 -0300

    feat: ​​🧩 applies url function from ValidationBuilder and removes urlValidator from constructor

--- src/presentation/controllers/create-short-url-controller.ts
+++ src/presentation/controllers/create-short-url-controller.ts
@@ -1,7 +1,6 @@
 import { ICreateShortUrlUseCase } from '@/domain/use-cases'
-import { InvalidParamError } from '@/presentation/errors'
-import { badRequest, created } from '@/presentation/helpers'
-import { IUrlValidator, ValidationBuilder as Builder, IValidation } from '@/presentation/validation'
+import { created } from '@/presentation/helpers'
+import { ValidationBuilder as Builder, IValidation } from '@/presentation/validation'
 import { HttpResponse } from '@/presentation/ports'
 import { Controller } from '@/presentation/controllers'
 
@@ -18,24 +17,19 @@ type CreateShortUrlResponse = {
 
 export class CreateShortUrlController extends Controller<CreateShortUrlRequest, CreateShortUrlResponse> {
   constructor (
-    private readonly urlValidator: IUrlValidator,
     private readonly createShortUrl: ICreateShortUrlUseCase
   ) {
     super()
   }
 
   async perform ({ originalUrl }: CreateShortUrlRequest): Promise<HttpResponse<CreateShortUrlResponse>> {
-    const isValid = this.urlValidator.isValid(originalUrl)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('originalUrl'))
-    }
     const result = await this.createShortUrl.perform({ originalUrl })
     return created(result)
   }
 
   override buildValidators ({ originalUrl }: CreateShortUrlRequest): IValidation[] {
     return [
-      ...Builder.of({ value: originalUrl, fieldName: 'originalUrl' }).required().build()
+      ...Builder.of({ value: originalUrl, fieldName: 'originalUrl' }).required().url().build()
     ]
   }
 }
