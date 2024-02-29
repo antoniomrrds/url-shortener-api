73af5aee8fa0969659c9e87a4607eda82bbf94a4
antoniomrrds <arqueiroverd@gmail.com>
2024-02-29 19:47:09 -0300

    refactor: 📦 move sut creation to a factory helper method

--- tests/main/adapters/express-route-adapter.spec.ts
+++ tests/main/adapters/express-route-adapter.spec.ts
@@ -3,26 +3,37 @@ import { getMockReq, getMockRes } from '@jest-mock/express'
 import { ControllerStub } from '@/tests/presentation/mocks'
 import { ExpressRouteAdapter } from '@/main/adapters'
 
+import { Request, Response } from 'express'
+
 jest.mock('@/tests/presentation/mocks')
+
+type SutTypes = {
+  sut: ExpressRouteAdapter
+  req: Request
+  res: Response
+  controllerSpy: ControllerStub
+}
+
+const makeSut = (): SutTypes => {
+  const req = getMockReq({ body: { anyBody: 'any_body' } })
+  const { res } = getMockRes()
+  const controllerSpy = new (ControllerStub as jest.MockedClass<typeof ControllerStub>)()
+
+  const sut = new ExpressRouteAdapter(controllerSpy)
+
+  return { sut, req, res, controllerSpy }
+}
 
 describe('ExpressRouteAdapter', () => {
   it('Should call handleRequest with correct request', async () => {
-    const req = getMockReq({ body: { anyBody: 'any_body' } })
-    const { res } = getMockRes()
-    const controllerSpy = new (ControllerStub as jest.MockedClass<typeof ControllerStub>)()
-
-    const sut = new ExpressRouteAdapter(controllerSpy)
-
+    const { sut, req, res, controllerSpy } = makeSut()
     await sut.adapt(req, res)
 
     expect(controllerSpy.handleRequest).toHaveBeenCalledWith({ anyBody: 'any_body' })
   })
   it('Should call handleRequest with empty request', async () => {
+    const { sut, res, controllerSpy } = makeSut()
     const req = getMockReq()
-    const { res } = getMockRes()
-    const controllerSpy = new (ControllerStub as jest.MockedClass<typeof ControllerStub>)()
-
-    const sut = new ExpressRouteAdapter(controllerSpy)
 
     await sut.adapt(req, res)
 
