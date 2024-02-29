d921f8b42bb9be420abd2d0738e422a4cb6c9bfe
antoniomrrds <arqueiroverd@gmail.com>
2024-02-29 06:04:22 -0300

    feat: ​​🧩 add routes config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
-import { setupMiddlewares } from '@/main/config'
+import { setupMiddlewares, setupRoutes } from '@/main/config'
+
 import express from 'express'
 
 const app = express()
 
 setupMiddlewares(app)
+setupRoutes(app)
 
 export { app }
--- src/main/config/index.ts
+++ src/main/config/index.ts
@@ -1,3 +1,4 @@
 export * from './module-alias'
 export * from './middlewares'
+export * from './routes'
 export * from './app'
