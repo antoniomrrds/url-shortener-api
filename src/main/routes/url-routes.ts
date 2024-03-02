3403edf2228e2d7272ff854db94f00c5f13c198e
antoniomrrds <arqueiroverd@gmail.com>
2024-03-02 01:06:07 -0300

    feat: ​​🧩 ensure that returns 201 and the data successfully

--- src/main/routes/url-routes.ts
+++ src/main/routes/url-routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/url', (req, res) => {
-    res.json({ ok: true })
+    res.status(201).json({ ok: true })
   })
 }
