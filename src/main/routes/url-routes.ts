7937eba9c1af85b8f7e272475d195992253b5392
antoniomrrds <arqueiroverd@gmail.com>
2024-03-03 19:45:56 -0300

    feat: ​​🧩 add  integration test to create short URL

--- src/main/routes/url-routes.ts
+++ src/main/routes/url-routes.ts
@@ -1,7 +1,7 @@
+import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { makeCreateShortUrlController } from '@/main/factories/presentation'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/url', (req, res) => {
-    res.status(201).json({ ok: true })
-  })
+  router.post('/url', adapt(makeCreateShortUrlController()))
 }
