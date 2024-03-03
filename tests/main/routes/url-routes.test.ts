b7bf01ce517535a7a625d12b3e67279c8bd6827e
antoniomrrds <arqueiroverd@gmail.com>
2024-03-03 19:46:25 -0300

    test: 🧪 add  integration test to create short URL

--- tests/main/routes/url-routes.test.ts
+++ tests/main/routes/url-routes.test.ts
@@ -1,12 +1,30 @@
+import { PgUrl } from '@/infrastructure/typeorm/entities'
 import { app } from '@/main/config'
+import { InMemoryDatabase } from '@/tests/infrastructure/typeorm/mocks'
 import request from 'supertest'
+import { DataSource } from 'typeorm'
 
 describe('Url Routes', () => {
+  let connection: DataSource
+
+  beforeAll(async () => {
+    connection = await InMemoryDatabase.connect([PgUrl])
+    jest.spyOn(DataSource.prototype, 'getRepository').mockReturnValue(connection.getRepository(PgUrl))
+  })
+
+  beforeEach(() => {
+    InMemoryDatabase.restore()
+  })
+
+  afterAll(async () => {
+    await connection.destroy()
+  })
+
   describe('POST api/url', () => {
     it('Should return 201 and the data successfully', async () => {
       const { statusCode } = await request(app)
         .post('/api/url')
-        .send({ url: 'http://www.google.com' })
+        .send({ originalUrl: 'https://www.google.com' })
 
       expect(statusCode).toBe(201)
     })
